3723c6a9078dd30ca67d25448e049a5dc2226382
ronald <ronaldgutierrezj@gmail.com>
2022-01-27 22:40:37 -0600

    first commit

---
+++ api/models/Cards.js
@@ -0,0 +1,40 @@
+/**
+ * Cards.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    image: {
+      type: 'string',
+      required: true,
+    },
+    name:{
+      type: 'string',
+      required: true,
+    },
+    cardNumber: {
+      type: 'string',
+      required: true,
+    },
+    collection:{
+      type: 'string',
+      required: true,
+    },
+    series: {
+      type: 'string',
+      required: true,
+    },
+    releaseDate:{
+      type: 'ref',
+      columnType: 'datetime',
+    }
+
+  },
+
+};
+
