fd48a1d4263779e668170ef35da67acb51bcf12c
ronald <ronaldgutierrezj@gmail.com>
2022-01-27 23:11:32 -0600

    2sd

--- config/env/production.js
+++ config/env/production.js
@@ -250,10 +250,10 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+    onlyAllowOrigins: [
+      'https://backend-toppcards.herokuapp.com',
+      'https://backend-toppcards.herokuapp.com',
+    ],
 
 
     /***************************************************************************
@@ -325,7 +325,7 @@ module.exports = {
     * (https://sailsjs.com/config/http)                                        *
     *                                                                          *
     ***************************************************************************/
-    // trustProxy: true,
+    trustProxy: true,
 
   },
 
